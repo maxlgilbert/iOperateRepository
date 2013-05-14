b1f167d832ddc7472e3fce87629c139d54b40c19
James Bartolozzi <bartolozzij@gmail.com>
2013-05-14 16:19:55 -0400

    boop

--- iOperate/UTLViewController.h
+++ iOperate/UTLViewController.h
@@ -2,7 +2,7 @@
 //  UTLViewController.h
 //  iOperate
 //
-//  Created by Max Gilbert on 5/13/13.
+//  Created by Max Gilbert who is a poop face on 5/13/13.
 //  Copyright (c) 2013 SIG Center. All rights reserved.
 //
 
